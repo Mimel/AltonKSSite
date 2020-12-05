8cacf551da7c1ff708aeb97acacbf64fea5c63ea
mimel <imel.matt.r@gmail.com>
2020-12-05 17:02:19 -0600

    Initial commit.

---
+++ src/main/java/ind/matti/AltonSiteApplication.java
@@ -0,0 +1,13 @@
+package ind.matti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltonSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltonSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ind/matti/AltonSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ind.matti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltonSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
